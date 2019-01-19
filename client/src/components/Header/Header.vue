e7086343590a05e07ff6c7341915368507b44b02
VictorPersson <vp-96@hotmail.com>
2019-01-19 01:08:24 +0100

    Implemented Header component, userName input PSOT to server & removed HelloWorld

---
+++ client/src/components/Header/Header.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <h1 class=headerH1> Buno </h1>
+    </div>
+</template>
+
+<script>
+export default {
+    function() {
+        return {
+
+        }
+    }
+}
+</script>
+
+<style>
+    .headerH1 {
+        font-size: 7rem;
+        font-weight: 200;
+        text-transform: uppercase;
+    }
+</style>
